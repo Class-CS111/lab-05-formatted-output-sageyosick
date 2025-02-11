5072eb0e13b40c8e730cef497c4f28d1b6c77261
sageyosick <sageyosick@gmail.com>
2025-02-11 17:41:49 +0000

    formatted output:)

--- Main.java
+++ Main.java
@@ -1,16 +1,35 @@
-// COLLABORATORS: <tutors, instructor, or anyone else who helped you>
+// COLLABORATORS: Sage Yosick
 
-// DESCRIPTION: <a sentence about what the purpose of this program is>
+// DESCRIPTION:
 
 public class Main
 {
 	public static void main(String[] args)
 	{
     //DECLARATION + INITIALIZATION SECTION
+    double initialAmount = 1.68;
+    int centsRemaining = 0;
+    int numQuarters = 0;
+    int numDimes = 0;
+    int numNickels = 0;
+    int numPennies = 0;
 
     //CALCULATION SECTION
+    centsRemaining = (int) (100 * initialAmount); 
+    numQuarters = centsRemaining / 25;
+    centsRemaining %= 25;
+    numDimes = centsRemaining / 10; 
+    centsRemaining %= 10;
+    numNickels = centsRemaining / 5;
+    centsRemaining %= 5;
 
     //OUTPUT SECTION
+    System.out.println(centsRemaining); 
+    System.out.println(numQuarters);
+    System.out.println(numDimes);
+    System.out.println(numNickels);
+
+    System.out.printf( "$%.2f can be converted to %d quarters, %d dimes, %d nicklels, and %d pennies. ", initialAmount, numQuarters, numDimes, numNickels, centsRemaining);
 
   }
 
